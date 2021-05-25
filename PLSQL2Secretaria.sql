5c59445e7706396a5fbcb2aad012403cb37e7a38
Roberto <rnginformatica@uma.es>
2021-05-25 19:13:39 +0200

    Terminado apartado e
    Falla la llamada del procedimiento 4 del anterior script

--- PLSQL2Secretaria.sql
+++ PLSQL2Secretaria.sql
@@ -23,7 +23,7 @@ ALTER TABLE GRUPOS_POR_ASIGNATURA ADD (NUM_ALUMNOS NUMBER(200), NUM_ALUMNOS_REAL
 
 --d 
 CREATE OR REPLACE TRIGGER ACTUALIZAR_ALUMNOS AFTER INSERT OR UPDATE OR DELETE ON ASIGNATURAS_MATRICULA
-FOR EACH ROW
+FOR EACH ROW --No se si esta bien, falta lo de el grupo_id not null (ESTO ESTA SOLUCIONADO CREO)
     BEGIN 
         if grupo_id is not null then
             IF INSERTING THEN
@@ -31,22 +31,18 @@ FOR EACH ROW
             ELSIF DELETING THEN
                 UPDATE GRUPOS_POR_ASIGNATURA SET NUM_ALUMNOS=NUM_ALUMNOS-1, NUM_ALUMNOS_REAL=NUM_ALUMNOS_REAL-1 WHERE GRUPO_ID=:NEW.GRUPO_ID;
             ELSE
-                UPDATE GRUPOS_POR_ASIGNATURA SET NUM_ALUMNOS=:OLD.NUM_ALUMNOS-1, NUM_ALUMNOS_REAL=:OLD.NUM_ALUMNOS_REAL-1 WHERE GRUPO_ID=:OLD.GRUPO_ID;
-                UPDATE GRUPOS_POR_ASIGNATURA SET NUM_ALUMNOS=:NEW.NUM_ALUMNOS+1, NUM_ALUMNOS_REAL=:NEW.NUM_ALUMNOS_REAL+1 WHERE GRUPO_ID=:NEW.GRUPO_ID;
-            END IF;
-        else
-            IF UPDATIN THEN
-                UPDATE GRUPOS_POR_ASIGNATURA SET NUM_ALUMNOS=:NEW.NUM_ALUMNOS+1, NUM_ALUMNOS_REAL=:NEW.NUM_ALUMNOS_REAL+1 WHERE GRUPO_ID=:NEW.GRUPO_ID;
+                UPDATE GRUPOS_POR_ASIGNATURA SET GRUPO_ID=:NEW.GRUPO_ID WHERE GRUPO_ID=:OLD.GRUPO_ID; --aqui no habria que igualar tambien el numero de alumnos??
             END IF;
         end if;
 END ACTUALIZAR_ALUMNOS;
-/ 
+/
+
 --e (EN DESARROLLO)
 create or replace package body PK_ASIGNACION_GRUPOS as
 procedure PR_ASIGNA_ASIGNADOS is 
     cursor alumnoCursor is select nuevo_ingreso.documento, alumnosext.documento from nuevo_ingreso inner join alumnosext 
         on nuevo_ingreso.documento = alumnoext.documento;
-    cursor asignaturaCursor is select * from asignatura_matricula;
+    cursor asignaturaCursor is select * from temp_asignaturas;
     pos number;
     pcadena varchar2(200);
     subcadena varchar2(20);
@@ -54,31 +50,24 @@ procedure PR_ASIGNA_ASIGNADOS is
     Acodigo varchar2(5);
     begin
         for al in alumnoCursor loop
-            
-            pcadena := al.alumnosext.grupos_asignados;
-            pos := instr(pcadena,',');--5
-            subcadena := substr(pcadena, 1, pos-1);   --"201-A"
-            exec normaliza_asignaturas(grupo_asignado, substr(al.expediente,1,4));--llamada al procedimiento de edu
-            --VAMOS A MODIFICAR ESTO PARA SACAR LOS DATOS DEL PROCEDIMIENTO
+            exec normaliza_asignaturas(al.grupo_asignado, substr(al.expediente,1,4));--llamada al procedimiento de edu
             
             for unAsig in asignaturaCursor loop
-                Acodigo := substr(subcadena, 1,3); --devuelve el codigo de la asignatura (201 por ejemplo)
-                letra := substr(subcadena, 5); --letra o null
+                letra := substr(unAsig.grupo_id,4);
                 if letra is null then 
                     insert into errores values(sysdate, 'No tiene letra del grupo', Acodigo, CURSO_ACTUAL(),null, null,al.expediente, substr(al.expediente,1,4));
-                    
-                else
-                    grupo := obten_grupo_id(substr(al.expediente,1,4), substr(subcadena,1), letra); 
-                    unAsig.grupo_id := (grupo);
-                    --asignatura_matricula.grupo_id := 1; --COMO SE MODIFICA EL GRUPO_ID??? QUE ESTA ALMACENANDO GRUPO_ID EXACATAMENTE???
-                    --insert into asignatura_matricula.
-                    
+                else 
+                    ALTER TABLE ASIGNATURA_MATRICULA ALTER COLUMN grupo_id (unAsig.grupo_id) 
+                        WHERE MATRICULA_EXPEDIENTES_NEXP LIKE al.expediente AND ASIGNATURA_REFERENCIA LIKE 
+                            (SELECT REFERENCIA FROM ASIGNATURA WHERE CODIGO LIKE unAsig.codigo);
                 end if;
             end loop;
             
         end loop;
-    end;      
-/            
+    end PR_ASIGNA_ASIGNADOS;      
+end PK_ASIGNACION_GRUPOS;
+/
+       
 -- f
 CREATE OR REPLACE FUNCTION LETRA_GRUPO_INGLES (NUMBER CTITULACION, NUMBER CASIGNATURA) 
 RETURN VARCHAR2 AS
